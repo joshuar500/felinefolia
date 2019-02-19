d8bea534671a79fa45a1b2230aebfc215d7bb843
Josh Rincon <memonkey@gmail.com>
2019-02-18 17:21:37 -0800

    Start deployment config

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -5,7 +5,21 @@ import os, os.path
 import pathlib
 import subprocess
 import digitalocean
+import paramiko
+import traceback
 import errno
+
+try:
+  import interactive
+except ImportError:
+  from . import interactive
+
+UseGSSAPI = (
+    paramiko.GSS_AUTH_AVAILABLE
+)  # enable "gssapi-with-mic" authentication, if supported by your python installation
+DoGSSAPIKeyExchange = (
+    paramiko.GSS_AUTH_AVAILABLE
+) # enable "gssapi-kex" key exchange, if supported by your python installation
 
 # TODO: Somebody make this look more pretty
 
@@ -47,6 +61,7 @@ def wait_for_ip(droplet):
       return droplet.ip_address
 
 def save_ip_to_file(ip_address):
+  # TODO: duplicate code
   path_to_save = pathlib.Path(__file__).parent / 'tmp'
   file_to_open = 'ip_address.txt'
   mkdir_p(path_to_save)
@@ -54,10 +69,57 @@ def save_ip_to_file(ip_address):
     f.write(ip_address)
 
 def get_ip_from_file():
-  pass
+  # TODO: duplicate code
+  path_to_open = pathlib.Path(__file__).parent / 'tmp'
+  file_to_open = 'ip_address.txt'
+  with open(path_to_open / file_to_open, 'r') as f:
+    ip_address = f.read()
+    return ip_address
 
 def deploy_to_droplet(ip_address):
-  pass
+  hostname = ip_address
+  port = 22
+  username = 'coreos'
+
+  try:
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.WarningPolicy())
+    print("*** Connecting...")
+    if not UseGSSAPI and not DoGSSAPIKeyExchange:
+        client.connect(hostname, port, username)
+    else:
+        try:
+            client.connect(
+                hostname,
+                port,
+                username,
+                gss_auth=UseGSSAPI,
+                gss_kex=DoGSSAPIKeyExchange,
+            )
+        except Exception:
+            # traceback.print_exc()
+            # password = getpass.getpass(
+            #     "Password for %s@%s: " % (username, hostname)
+            # )
+            client.connect(hostname, port, username)
+
+    client.exec_command('echo im connected remotely')
+    # chan = client.invoke_shell()
+    # print(repr(client.get_transport()))
+    # print("*** Here we go!\n")
+    # interactive.interactive_shell(chan)
+    # chan.close()
+    client.close()
+
+  except Exception as e:
+      print("*** Caught exception: %s: %s" % (e.__class__, e))
+      traceback.print_exc()
+      try:
+          client.close()
+      except:
+          pass
+  sys.exit(1)
 
 # Taken from https://stackoverflow.com/a/600612/119527
 def mkdir_p(path):
@@ -77,13 +139,19 @@ def safe_open_w(path):
 def main(argv):
   if argv[1] == 'create_droplet':
     try:
+      print('Creating droplet...')
       droplet = create_droplet()
       ip_address = wait_for_ip(droplet)
       save_ip_to_file(ip_address)
     except TimeoutError:
-      print('Creating droplet timed out')
+      print('ERROR: Creating droplet timed out')
   if argv[1] == 'deploy_to_staging':
-    print('deploying to staging environment')
+    try:
+      print('Deploying to staging...')
+      ip_address = get_ip_from_file()
+      deploy_to_droplet(ip_address)
+    except TimeoutError:
+      print('ERROR: Deploying to Staging')
     
 
 if __name__ == '__main__':
